6ac0485ccd508842648423cb39809adcd50b8798
tommytommychopper <t.itsuki928@gmail.com>
2021-09-23 19:59:34 -0700

    :truck: chore(add commitizen to commit using emoji): :truck: chore: add commitizen to commit using emoji

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-  return <h1 className='text-blue-500'>Begin Project!</h1>
+	return (
+		<>
+			<h1 className="text-blue-500">Begin Project!</h1>
+		</>
+	)
 }
 
 export default Home
