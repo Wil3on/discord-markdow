744f9daacbf3f06a6dcc73f2367604ff9232f73e
tommytommychopper <t.itsuki928@gmail.com>
2021-09-24 13:24:31 -0700

    :construction: wip: finished textarea editor

--- components/MarkdownEditor.tsx
+++ components/MarkdownEditor.tsx
@@ -1,29 +1,31 @@
 import { useState } from 'react'
 import TextareaAutosize from 'react-textarea-autosize'
 import { FaUserCircle } from 'react-icons/fa'
+import { IoMdAddCircle } from 'react-icons/io'
 
 export const MarkdownEditor: React.VFC = () => {
 	const [text, setText] = useState<string>('')
 	return (
 		<section className="bg-navy">
-			<div className="flex justify-center flex-col md:flex-row">
-				<div className="flex-1 m-3 max-w-5xl">
+			<div className="flex flex-col justify-center md:flex-row">
+				<div className="flex flex-1 max-w-5xl px-4 py-6 m-3 rounded-lg bg-navy-light">
+					<IoMdAddCircle size={35} className="text-gray" />
 					<TextareaAutosize
-						className="bg-navy-light w-full px-4 py-6 rounded-lg text-while placeholder-gray-light outline-none"
+						className="w-full pt-1 ml-3 text-xl outline-none bg-navy-light text-while placeholder-gray-light"
 						placeholder="Message"
 						value={text}
 						onChange={(e) => setText(e.target.value)}
 					/>
 				</div>
-				<div className="flex-1 m-3 max-w-5xl">
-					<div className="bg-navy-light p-4 rounded-lg text-while flex  ">
+				<div className="flex-1 max-w-5xl m-3">
+					<div className="flex p-4 rounded-lg bg-navy-light text-while ">
 						<FaUserCircle size={40} className="text-gray min-w-[40px]" />
 						<div className="ml-3">
 							<h4 className="text-xl font-light">
 								you
 								<span className="ml-2 text-sm text-gray-light">Today at 11:30 PM</span>
 							</h4>
-							<p className="whitespace-pre-line">{text}</p>
+							<p className="text-xl whitespace-pre-line">{text}</p>
 						</div>
 					</div>
 				</div>
