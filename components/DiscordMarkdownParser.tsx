3be69fe9c49ef90dbfea535f129ddcdc9d0f4b0a
tommytommychopper <t.itsuki928@gmail.com>
2021-09-25 15:10:02 -0700

    :construction: wip: added italic and backquote functionality

--- components/DiscordMarkdownParser.tsx
+++ components/DiscordMarkdownParser.tsx
@@ -6,9 +6,15 @@ type DiscordMarkdownParserProps = {
 }
 
 const convertDiscordMarkdown = (message: string) => {
-	const htmlText = message.replace(/^# (.*$)/gim, '<b>$1</b>')
-	console.log(htmlText)
-	return htmlText
+	const markdownedMessage = message
+		.replace(/\*\*(.*)\*\*/gim, '<b>$1</b>') // bold text
+		.replace(/\*(.*)\*/gim, '<i>$1</i>') // italic text
+		.replace(
+			/^\>(.*$)/gim,
+			'<backquote className="inline-block pl-2 border-l-4 border-gray-light">$1</backquote>'
+		) // blockquote
+	console.log(markdownedMessage)
+	return markdownedMessage
 }
 
 export const DiscordMarkdownParser: React.VFC<DiscordMarkdownParserProps> = ({ message }) => {
