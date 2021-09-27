1f733ce954b7f0fb045a13c6878c17ac390d6d20
tommytommychopper <t.itsuki928@gmail.com>
2021-09-27 14:44:42 -0700

    :lipstick: style: adjust heading font size for iphone 5s

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ const Home: NextPage = () => {
 		<div className="h-screen px-3 py-10">
 			<h1 className="flex flex-col items-center justify-center mb-1 md:mb-3 md:flex-row">
 				<FaDiscord size={40} className="text-blue" />
-				<span className="mt-1 text-xl font-bold md:mt-0 md:ml-3 md:text-2xl text-while">
+				<span className="mt-1 text-lg font-bold md:mt-0 md:ml-3 md:text-2xl text-while">
 					Discord Markdown Live Previewer
 				</span>
 			</h1>
