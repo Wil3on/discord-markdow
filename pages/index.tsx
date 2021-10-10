2f5191196898b87e23fcf34a3332aebb73ed7744
tommytommychopper <t.itsuki928@gmail.com>
2021-10-10 07:31:28 -0700

    :bug: fix: fixed left and right padding

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import type { NextPage } from 'next'
 import { MarkdownEditor } from '../components/MarkdownEditor'
 import { Section } from '../components/Section'
@@ -7,79 +8,81 @@ import { CardGrid } from '../components/CardGrid'
 
 const Home: NextPage = () => {
 	return (
-		<div className="pt-10 pb-10">
-			<header>
-				<h1 className="flex flex-col items-center justify-center mb-1 md:mb-3 md:flex-row">
-					<FaDiscord size={40} className="text-blue" />
-					<span className="mt-1 text-lg font-bold text-white md:mt-0 md:ml-3 md:text-2xl">
-						Discord Markdown Live Previewer
-					</span>
-				</h1>
-			</header>
-			<MarkdownEditor />
-			<Section heading="About">
-				<p className="pt-2 text-base text-white">
-					This app will allow you to see the actual message you&apos;ll send on discord. The reason behind creating this
-					app is whenever I want to format my text on discord, such as making the text bold or colorized. I am not
-					always 100% sure if the markdown will output the way I want it to be. So, I needed something to preview my
-					markdown before submitting it.
-				</p>
-			</Section>
-			<Section heading="Feature">
-				<div className="pt-7" />
-				<CardGrid>
-					<Card input={'*Italics*'} />
-					<Card input={'**Bold**'} />
-					<Card input={'***Bold Italics***'} />
-					<Card input={'__Underline__'} />
-					<Card input={'__*Underline Italics*__'} />
-					<Card input={'__**Underline Bold**__'} />
-					<Card input={'__***Underline Bold Italics***__'} />
-					<Card input={'~~Strikethrough~~'} />
-					<Card input={`>Block Quotes`} />
-					<Card
-						input={`>>>Multiline
+		<>
+			<div className="px-2 pt-10 pb-10 md:px-4">
+				<header>
+					<h1 className="flex flex-col items-center justify-center mb-1 md:mb-3 md:flex-row">
+						<FaDiscord size={40} className="text-blue" />
+						<span className="mt-1 text-lg font-bold text-white md:mt-0 md:ml-3 md:text-2xl">
+							Discord Markdown Live Previewer
+						</span>
+					</h1>
+				</header>
+				<MarkdownEditor />
+				<Section heading="About">
+					<p className="pt-2 text-base text-white">
+						This app will allow you to see the actual message you&apos;ll send on discord. The reason behind creating
+						this app is whenever I want to format my text on discord, such as making the text bold or colorized. I am
+						not always 100% sure if the markdown will output the way I want it to be. So, I needed something to preview
+						my markdown before submitting it.
+					</p>
+				</Section>
+				<Section heading="Feature">
+					<div className="pt-7" />
+					<CardGrid>
+						<Card input={'*Italics*'} />
+						<Card input={'**Bold**'} />
+						<Card input={'***Bold Italics***'} />
+						<Card input={'__Underline__'} />
+						<Card input={'__*Underline Italics*__'} />
+						<Card input={'__**Underline Bold**__'} />
+						<Card input={'__***Underline Bold Italics***__'} />
+						<Card input={'~~Strikethrough~~'} />
+						<Card input={`>Block Quotes`} />
+						<Card
+							input={`>>>Multiline
 Block Quotes`}
-					/>
-					<Card input={'***Bold Italics***'} />
-					<Card input={'`Code Blocks`'} />
-					<Card
-						input={`\`\`\`MultiLine
+						/>
+						<Card input={'***Bold Italics***'} />
+						<Card input={'`Code Blocks`'} />
+						<Card
+							input={`\`\`\`MultiLine
 Code Blocks\`\`\``}
-					/>
-					<Card
-						input={`\`\`\`diff
+						/>
+						<Card
+							input={`\`\`\`diff
 - Red 
 \`\`\``}
-					/>
-					<Card
-						input={`\`\`\`fix
+						/>
+						<Card
+							input={`\`\`\`fix
 Yellow
 \`\`\``}
-					/>
-					<Card
-						input={`\`\`\`css
+						/>
+						<Card
+							input={`\`\`\`css
 [ Orange ]
 \`\`\``}
-					/>
-					<Card
-						input={`\`\`\`diff
+						/>
+						<Card
+							input={`\`\`\`diff
 + Light Green 
 \`\`\``}
-					/>
-					<Card
-						input={`\`\`\`bash
+						/>
+						<Card
+							input={`\`\`\`bash
 "Light Blue"
 \`\`\``}
-					/>
-					<Card
-						input={`\`\`\`ini
+						/>
+						<Card
+							input={`\`\`\`ini
 [ Dark Blue ]
 \`\`\``}
-					/>
-				</CardGrid>
-			</Section>
-		</div>
+						/>
+					</CardGrid>
+				</Section>
+			</div>
+		</>
 	)
 }
 
